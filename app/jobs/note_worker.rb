c964858c3e888bcd22cec71adb19319ecab55bec
Alexey Kisel <kisel.alexey@gmail.com>
2021-04-17 22:58:02 +0300

    sidekiq change

--- app/jobs/note_worker.rb
+++ app/jobs/note_worker.rb
@@ -3,7 +3,7 @@ class NoteWorker
 
   def perform(id)
     note = Note.find(id)
-    note.text = note.text + " [processed]"
+    note.text = note.text + " [processed in sidekiq]"
     note.save!
   end
 end
